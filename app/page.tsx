9007bba5529b07e2ce840aad42e80adc7d0e97e4
Ahmed-Nasser-99 <forall3020@gmail.com>
2023-08-18 14:51:08 +0300

    Implement the homepage header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-import React from "react";
-
 const page = () => {
-  return <div>page</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Find & Share <br className="hidden max-md:visible max-md:block" />
+        <span className="orange_gradient text-center">AI Powered Prompts</span>
+      </h1>
+
+      <p className="desc text-center ">
+        Promptopia is an open source project that allows you to find and share
+        AI powered prompts.
+      </p>
+    </section>
+  );
 };
 
 export default page;
